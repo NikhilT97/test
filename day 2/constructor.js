5c22d51e728bec5848a34dda91363387ea4079e5
NikhilT97 <nikhiltayde97@gmail.com>
2022-06-26 20:19:02 +0530

    trial commit

---
+++ day 2/constructor.js
@@ -0,0 +1,22 @@
+
+
+
+
+function myProd(n,t,p){
+
+    let obj = {}
+       obj.name =n;
+       obj.type =t;
+        obj.price=p;
+
+    return obj;
+}
+
+
+let x =  myProd('shoe', 'sneaker', 2000);
+
+ console.log(x)
+
+
+
+// create ecom prod db by acceeting values through FormD
---
+++ day3/proto.js
@@ -0,0 +1,14 @@
+
+// create method that should work with all the arrays present in js 
+
+let arr1 = [1,2,3,4,5];
+
+
+
+let arr2 = [4,5,4,2,6,7];
+
+Array.prototype.जयभीम = function() {
+    console.log(`Dr. Babasaheb Ambedkarancha`)
+};
+
+arr1.जयभीम();
